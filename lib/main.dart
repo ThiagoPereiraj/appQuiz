fd13d3524472348971a34ef08c05f495867ab87a
Thiago Pereira <thiagopereiaj@hotmail.com>
2023-11-28 20:46:17 -0300

    estrutura criada

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(QuizApp());
+
+class QuizApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        const Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'As perguntas serão exibidas aqui.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                 
+              backgroundColor: Colors.deepPurple,
+              ),
+             
+              child: const Text(
+                'Verdadeiro',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //O usuário clica no botão verdadeiro.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                 
+              backgroundColor: Colors.grey.shade800,
+              ),
+              child: const Text(
+                'Falso',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //O usuário clica no botão falso.
+              },
+            ),
+          ),
+        ),
+         Row(
+          children: [
+            Icon(Icons.check),
+          ],
+         )
+         
+      ],
+    );
+  }
+}
+
+/*
+pergunta1: 'O metrô é um dos meios de transporte mais seguros do mundo', verdadeiro,
+pergunta2: 'A culinária brasileira é uma das melhores do mundo.', verdadeiro,
+pergunta3: 'Vacas podem voar, assim como peixes utilizam os pés para andar.', falso,
+*/
